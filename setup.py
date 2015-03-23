115bd045f989101738976607a161f48f3a68714c
Eric Dill <edill@bnl.gov>
2015-03-23 17:54:41 -0400

    MNT: Still in initial setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import warnings
+
+
+from setuptools import setup
+
+setup(
+    name='controlsui',
+    version='0.0.0',
+    author='Brookhaven National Laboratory',
+    packages=['controlsui'],
+)
